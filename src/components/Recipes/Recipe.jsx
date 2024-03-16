880d7317f4a3040e443c99bce8809cce468af948
Rafidul01 <rafidulhasan555@gmail.com>
2024-03-16 19:56:48 +0600

    recipe json data fetched and made cards dynamic

--- src/components/Recipes/Recipe.jsx
+++ src/components/Recipes/Recipe.jsx
@@ -1,27 +1,30 @@
+/* eslint-disable react/prop-types */
 import { MdOutlineWatchLater } from "react-icons/md";
 import { RiFireLine } from "react-icons/ri";
-const Recipe = () =>{
+const Recipe = ({card}) =>{
+  // eslint-disable-next-line react/prop-types
+  const{recipe_name, short_description, ingredients, preparing_time, calories, recipe_image} = card;
     return(
       <div className="card lg:w-96 bg-base-100 shadow-xl border border-[#2828281A]">
   <figure className="p-6">
-    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+    <img src={recipe_image} alt="Shoes" className="rounded-xl h-[200px] w-full" />
   </figure>
   <div className="card-body pt-0 space-y-4">
-    <h2 className="card-title text-[#282828] font-bold text-[20px] lexend ">Shoes!</h2>
-    <p className="text-[#878787] font-normal text-base fira-sans">If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title text-[#282828] font-bold text-[20px] lexend ">{recipe_name}</h2>
+    <p className="text-[#878787] font-normal text-base fira-sans">{short_description}</p>
     <hr className="border-[#2828281A]" />
     <div>
-      <h1 className="lexend text-[#282828] text-lg font-medium mb-4">Ingredients: 6</h1>
+      <h1 className="lexend text-[#282828] text-lg font-medium mb-4">Ingredients: {ingredients.length}</h1>
       <ul className="list-disc fira-sans text-[#878787] text-lg ml-6 font-normal">
-      <li>500g ground beef</li>
-      <li>500g ground beef</li>
-      <li>500g ground beef</li>
+        {
+          ingredients.map(ingredient => <li key={ingredient.id}>{ingredient}</li>)
+        }
     </ul>
     </div>
     <hr className="border-[#2828281A]" />
     <div className="flex justify-between text-[#282828CC] fira-sans font-normal">
-      <p className="flex items-center gap-4"><MdOutlineWatchLater /> 30 minutes</p>
-      <p className="flex items-center gap-4"><RiFireLine /> 600 calories</p>
+      <p className="flex items-center gap-4"><MdOutlineWatchLater /> {preparing_time} minutes</p>
+      <p className="flex items-center gap-4"><RiFireLine /> {calories} calories</p>
     </div>
     <div className="card-actions">
       <button className="btn bg-[#0be58a] rounded-[50px] text-[#150B2B] font-medium text-lg">Want to Cook</button>
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,16 +1,23 @@
 import Title from "./Title";
 import Recipe from "./Recipe";
 import Cart from "./Cart";
+import { useEffect, useState } from "react";
 const Recipes = () =>{
+    const[recipes,setRecipes] = useState([]);
+    useEffect(()=>{
+        fetch('/recipes.json')
+        .then((res) => res.json())
+        .then(data => setRecipes(data));
+    },[])
+    
     return(
         <div className="container mx-auto">
             <Title></Title>
-
             <div className="flex flex-col lg:flex-row justify-between gap-6 ">
                 <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto gap-6 w-full lg:w-2/3">
-                    <Recipe></Recipe>
-                    <Recipe></Recipe>
-                    <Recipe></Recipe>
+                    {
+                        recipes.map(card => <Recipe key={card.recipe_id} card={card}></Recipe>)
+                    }   
                 </div>
                 <div className="w-full lg:w-1/3 border border-[#28282826] py-[32px] rounded-2xl">
                     <Cart></Cart>
